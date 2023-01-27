0761ef9263ae83a6294c0878b23f89ef0d411207
crushjunior <charushnikovvadim@gmail.con>
2023-01-27 22:00:37 +0300

    не проходит тест findStudentByAgeBetween

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,7 +1,10 @@
 package ru.hogwarts.school;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest
 class SchoolApplicationTests {
